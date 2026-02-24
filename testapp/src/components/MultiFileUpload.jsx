16578e3c58155d714928939812ffabcd5af29817
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-24 20:28:24 +0530

    update aws upload

--- testapp/src/components/MultiFileUpload.jsx
+++ testapp/src/components/MultiFileUpload.jsx
@@ -14,7 +14,7 @@ export default function MultiFileUpload() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/upload/multiple",
+        "http://43.205.215.233/api/upload/multiple",
         formData
       );
 
